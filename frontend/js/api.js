316e6f50c51acd167256d499c8d857e05f196b07
Nomusa Shongwe <shongwenomusa990822@gmail.com>
2026-09-16 11:24:25 +0200

    Create api.js

---
+++ frontend/js/api.js
@@ -0,0 +1,35 @@
+const API_BASE_URL = "http://127.0.0.1:8000/api";
+
+
+async function apiRequest(
+    endpoint,
+    options = {}
+) {
+    const response = await fetch(
+        `${API_BASE_URL}${endpoint}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                ...(options.headers || {})
+            },
+
+            ...options
+        }
+    );
+
+    const contentType =
+        response.headers.get("content-type") || "";
+
+    const data = contentType.includes("application/json")
+        ? await response.json()
+        : await response.text();
+
+    if (!response.ok) {
+        throw new Error(
+            data?.detail ||
+            "Something went wrong."
+        );
+    }
+
+    return data;
+}
